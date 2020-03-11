f35f950f2cec0951fd56a128cd36218b37acb0cf
Jamie Fernandez <jamie.fernandez@imtins.com>
2020-03-10 22:17:31 -0500

    Adds bootstrap for vue, renames Input.vue to Keyboard.vue, and adds button text values

---
+++ src/components/Keyboard.vue
@@ -0,0 +1,50 @@
+<template>
+      <!-- TI-108 Elementary Calculator Keys-->
+    <div class="calculator-keys">
+        <row>
+            <button>+/-</button>
+            <button>√</button>
+            <button>%</button>
+            <button>+</button>
+        </row>
+        <row>
+            <button>MRC</button>
+            <button>M-</button>
+            <button>M+</button>
+            <button>X</button>
+        </row>
+        <row>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+        </row>
+        <row>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+        </row>
+        <row>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+        </row>
+        <row>
+            <button>ON/C</button>
+            <button>0</button>
+            <button>.</button>
+        </row>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+      <Keyboard/>
+  </div>
+</template>
+
+<script>
+import Keyboard from '@/components/Keyboard.vue'
+export default {
+    name: 'Calculator',
+    components: {
+        Keyboard,
+    },
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style>
+
+</style>
